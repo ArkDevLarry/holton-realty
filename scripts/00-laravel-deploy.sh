ec1a05bebf62a8e22b3a3408ad1fceb3aeb0422c
Akerele Quadri <akereleayomide106@gmail.com>
2025-03-08 11:57:13 +0100

    Pushe dockfil

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 echo "Running composer"
 composer global require hirak/prestissimo
-composer install --no-dev --working-dir=/var/www/html
 
-echo "skip generating application key..."
+# Increase memory limit for composer
+COMPOSER_MEMORY_LIMIT=-1 composer install --no-dev --working-dir=/var/www/html
+
+echo "Skip generating application key..."
 # php artisan key:generate --show
 
 echo "Caching config..."
